0e833b077345ab1600f6cfe4cef3f24139a2215e
GermanDavid <david.german@post.idc.ac.il>
2025-01-31 10:43:24 +0200

    update

--- components/chat/chat-help.tsx
+++ components/chat/chat-help.tsx
@@ -1,6 +1,5 @@
 import useHotkey from "@/lib/hooks/use-hotkey"
 import {
-  IconBrandGithub,
   IconBrandX,
   IconHelpCircle,
   IconQuestionMark
@@ -31,27 +30,7 @@ export const ChatHelp: FC<ChatHelpProps> = ({}) => {
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end">
-        <DropdownMenuLabel className="flex items-center justify-between">
-          <div className="flex space-x-2">
-            <Link
-              className="cursor-pointer hover:opacity-50"
-              href="https://twitter.com/ChatbotUI"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <IconBrandX />
-            </Link>
-
-            <Link
-              className="cursor-pointer hover:opacity-50"
-              href="https://github.com/mckaywrigley/chatbot-ui"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <IconBrandGithub />
-            </Link>
-          </div>
-
+        <DropdownMenuLabel className="flex items-center justify-end">
           <div className="flex space-x-2">
             <Announcements />
 
